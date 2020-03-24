8cd22f2db1da3398bba54edebb0b0c46e61517fb
Sonja Parsell <hello@sparsell.com>
2020-03-24 12:01:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,14 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  colors_array = ["reds", "blues", "greens", "oranges"]
-  element = ["purples"]
-  colors_array << element
-
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-    array << element
+    array.unshift element
 end
 
 def remove_element_from_end_of_array(array)
