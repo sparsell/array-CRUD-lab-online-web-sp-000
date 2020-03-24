cd0bd6a51c1ddeb28409de80e55722971145211f
Sonja Parsell <hello@sparsell.com>
2020-03-24 11:50:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,6 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   colors_array = ["reds", "blues", "greens", "oranges"]
   element = ["purples"]
+  colors_array << element
 end
 
 def add_element_to_start_of_array(array, element)
